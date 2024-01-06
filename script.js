39416eadedf0c26c15d3544bb97d37aef309a7ef
Nyisztor Zoltan <znyisztor77@gmail.com>
2024-01-06 11:17:43 +0100

    Login oldal hozzáadása.

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+function login() {
+    var username = document.getElementById("username").value;
+    var psw = document.getElementById("password").value;
+   
+    if(username == "admin" && psw == "admin")
+    {
+        alert("Sikeres bejelentkezés!")
+        
+        window.open('index.html');
+        
+        
+    }
+    else{
+        
+        alert("Hibás adatok!  Kérem adja meg a helyes adatokat");
+        document.getElementById("loginForm").reset();
+        
+        
+    }
+    
+}
+
