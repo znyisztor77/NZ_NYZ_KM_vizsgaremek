48a924151acfe43eba0c6502b5a3dd00073ecec2
nyisztor.zoltan <znyisztor77@gmail.com>
2024-01-08 16:35:50 +0100

    javascript modosítás

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,30 @@
-
-
+var username = document.getElementById("username");
+var psw = document.getElementById("password");
 
 function login() {
-    var username = document.getElementById("username").value;
-    var psw = document.getElementById("password").value;
-   
-    if(username == "Admin" && psw == "1234"){
+    if(username.value == "Admin" && psw.value == "1234"){
         alert("Sikeres bejelentkezés Admin!")
         
         window.open('admin.html');
         
         
-    }else if(username == "Raktár" && psw == "1234"){
+    }else if(username.value == "Raktár" && psw.value == "1234"){
         alert("Sikeres bejelentkezés Raktár!")
         
         window.open('raktar.html');
         
 
-    }else if(username == "Béla" && psw == "1234"){
+    }else if(username.value == "Béla" && psw.value == "1234"){
         alert("Sikeres bejelentkezés Béla!")
-        
         window.open('operator.html');
-
     }
     else{
-        
         alert("Hibás adatok!  Kérem adja meg a helyes adatokat");
         document.getElementById("loginForm").reset();
-        
-        
     }
     
 }
 function closedTab() {
     window.open('login.html');
     this.close();
-  }
+}
