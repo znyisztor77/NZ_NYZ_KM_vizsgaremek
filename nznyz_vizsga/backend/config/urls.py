eb13c22cbeea4cc8d2dbbd3cd9bb803b8a82652d
Nyisztor Zoltan <znyisztor77@gmail.com>
2024-01-27 20:35:10 +0100

    leírás

--- nznyz_vizsga/backend/config/urls.py
+++ nznyz_vizsga/backend/config/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 
 from django.shortcuts import render
+from raktar_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/megrendelesek', views.getMegrendelesek )
     #path('', main),
 ]
--- nznyz_vizsga/backend/raktar_app/views.py
+++ nznyz_vizsga/backend/raktar_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Megrendelesek
+from .serializers import MegrendelesekSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def getMegrendelesek(request):
+    megrendelesek =Megrendelesek.objects.all()
+    serialized =MegrendelesekSerializer(megrendelesek, many =True)
+
+    return Response(serialized.data)
