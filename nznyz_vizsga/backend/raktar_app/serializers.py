70654755b803e9aae98452b4f27bafca58eb1501
Nyisztor Zoltan <znyisztor77@gmail.com>
2024-02-17 14:37:36 +0100

    Api módosítás

--- nznyz_vizsga/backend/raktar_app/serializers.py
+++ nznyz_vizsga/backend/raktar_app/serializers.py
@@ -1,10 +1,13 @@
 from .models import Megrendelesek
+from django.db import models
 from rest_framework.serializers import ModelSerializer
 
 class MegrendelesekSerializer(ModelSerializer):
+    
     class Meta:
-        fields = '__all__'
+        
+        fields = ['dolgozo', 'munkalap_szama', 'alapanyag','datumKezdes','datumBefejezes', 'felhasznaltMennyiseg']
         model = Megrendelesek
-        depth = 2
+        #depth = 1
         
         
--- nznyz_vizsga/backend/raktar_app/urls.py
+++ nznyz_vizsga/backend/raktar_app/urls.py
@@ -7,13 +7,13 @@ urlpatterns = [
     path('login/', views.bejelentkezes, name ='login'),
     path('logout/', views.logout_page, name='logout'),
     path('',views.home, name = 'home'),
-    path('megrendelesek/', HomeView.as_view(), name= 'home'),
     path('raktar/' ,views.bevitel_kiadas, name='raktar'),
     path('megrendeles/', views.megrendeles, name= 'megrendeles'),
 
 
     
    
+    #path('megrendelesek/', HomeView.as_view(), name= 'home'),
     #path('raktar/', RaktarView.as_view(), name= 'raktar'),
     #path('dolgozo/', DolgozoView.as_view(), name= 'dolgozo'),
     #path('',HomeView.as_view(), name = 'home'),
